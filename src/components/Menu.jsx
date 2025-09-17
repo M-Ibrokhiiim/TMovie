1dd823e6e7f4dbf9586d668766f2ee4e960362d2
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-09-17 21:24:05 +0500

    Done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,12 +6,12 @@ import Insta from '../assets/PICS/INSTA.png'
 function Menu(){
     return(
         <>
-        <div  className='xl:w-[130px] xl:h-[200px] sm:w-[40px] lg:w-[40px] md:w-[20px] 2xl:right-[100px]  w-[20px] right-18  z-90 md:right-[100px] sm:-right-[1px] md:top-[20px] sm:top-[120px] fixed'>
+        <div  className='xl:w-[130px] xl:h-[200px] sm:w-[80px] lg:w-[40px] md:w-[20px] 2xl:right-[100px]  w-[45px] right-14  z-90 md:right-[100px] sm:-right-[1px] md:top-[20px] sm:top-[120px] fixed'>
                     <div className='relative'>
-                         <img className='lg:w-[21px] lg:h-[21px]  md:w-[10px]   absolute  left-[7px]  cursor-pointer hover:scale-110 active:scale-90 duration-150  rounded-3xl ' src={TG}/>
-                         <img className='lg:w-[25px] lg:h-[25px]  md:w-[14px]   absolute top-[40px] -left-[6px]  rounded-2xl cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Phone }/>
-                         <img className='lg:w-[40px] lg:h-[37px]  md:w-[20px]    md:h-[22px] absolute top-[85px] left-[4px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150 rounded-3xl' src={Insta}/>
-                         <img className='lg:w-[45px] lg:h-[40px]  absolute       top-[130px] left-[45px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Whatsapp}/>
+                         <img className='lg:w-[21px] lg:h-[21px]   sm:w-[15px] w-[12px]  absolute  sm:left-[7px] left-[7px]  cursor-pointer hover:scale-110 active:scale-90 duration-150  rounded-3xl ' src={TG}/>
+                         <img className='lg:w-[25px] lg:h-[25px]   sm:w-[20px] w-[18px] absolute sm:top-[40px] top-[40px] sm:-left-[6px] -left-[1px  rounded-2xl cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Phone }/>
+                         <img className='lg:w-[40px] lg:h-[37px]   sm:w-[25px]  sm:h-[35px] w-[40px] h-[22px] absolute sm:top-[85px] top-[85px] sm:left-[4px] left-[4px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150 rounded-3xl' src={Insta}/>
+                         <img className='lg:w-[45px] lg:h-[40px]   sm:w-[50px] sm:h-[40px] absolute       top-[130px] sm:left-[45px] left-[45px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Whatsapp}/>
                     </div>
         </div> 
         </>
