9d3e40ae91b4db650e1b8d79616c1a2714455a8f
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-09-17 21:31:06 +0500

    Done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,9 +8,9 @@ function Menu(){
         <>
         <div  className='xl:w-[130px] xl:h-[200px] sm:w-[80px] lg:w-[40px] md:w-[20px] 2xl:right-[100px]  w-[45px] right-14  z-90 md:right-[100px] sm:-right-[1px] md:top-[20px] sm:top-[120px] fixed'>
                     <div className='relative'>
-                         <img className='lg:w-[21px] lg:h-[21px]   sm:w-[15px] w-[12px]  absolute  sm:left-[7px] left-[7px]  cursor-pointer hover:scale-110 active:scale-90 duration-150  rounded-3xl ' src={TG}/>
-                         <img className='lg:w-[25px] lg:h-[25px]   sm:w-[20px] w-[18px] absolute sm:top-[40px] top-[40px] sm:-left-[6px] -left-[1px  rounded-2xl cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Phone }/>
-                         <img className='lg:w-[40px] lg:h-[37px]   sm:w-[25px]  sm:h-[35px] w-[40px] h-[22px] absolute sm:top-[85px] top-[85px] sm:left-[4px] left-[4px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150 rounded-3xl' src={Insta}/>
+                         <img className='lg:w-[21px] lg:h-[21px]   sm:w-[15px] w-[16px]  absolute  sm:left-[7px] left-[12px]  cursor-pointer hover:scale-110 active:scale-90 duration-150  rounded-3xl ' src={TG}/>
+                         <img className='lg:w-[25px] lg:h-[25px]   sm:w-[20px] w-[19px] absolute sm:top-[40px] top-[40px] sm:-left-[6px] left-[4px]  rounded-2xl cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Phone }/>
+                         <img className='lg:w-[40px] lg:h-[37px]   sm:w-[25px]  sm:h-[35px] w-[40px] h-[22px] absolute sm:top-[85px] top-[85px] sm:left-[4px] left-[6px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150 rounded-3xl' src={Insta}/>
                          <img className='lg:w-[45px] lg:h-[40px]   sm:w-[50px] sm:h-[40px] absolute       top-[130px] sm:left-[45px] left-[45px] cursor-pointer  shadow-md hover:scale-110 active:scale-90 duration-150' src={Whatsapp}/>
                     </div>
         </div> 
